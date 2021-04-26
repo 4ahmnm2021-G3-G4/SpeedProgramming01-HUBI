11f0fcef25a2ecd5effb6b617ad71f65df8f6ae8
“hubi7676” <hubert.kreutzer02@htl-salzburg.ac.at>
2021-04-26 10:00:38 +0200

    add Calculator Buttons + Calculator Script

---
+++ Assets/MyGame/Scripts/AddNumbers.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AddNumbers : MonoBehaviour
+{
+    public int NumberA;
+    public int NumberB;
+
+    public Text resultText;
+    public InputField inputA;
+    public InputField inputB;
+        
+    void Update()
+    {
+        NumberA = int.Parse(inputA.text);
+        NumberB = int.Parse(inputB.text);
+    }
+
+    public void CalculateNumbers ()
+    {
+        var result = NumberA + NumberB;
+        resultText.text = "= " + result.ToString();
+        
+    }
+}
