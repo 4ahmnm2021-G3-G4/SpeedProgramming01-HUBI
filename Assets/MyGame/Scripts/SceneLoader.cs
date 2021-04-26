a93b53b9045315c122adab79ce74215298befd85
“hubi7676” <hubert.kreutzer02@htl-salzburg.ac.at>
2021-04-26 09:23:36 +0200

    add Script

---
+++ Assets/MyGame/Scripts/SceneLoader.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadMainScene()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+
+    public void LoadWelcomeScene()
+    {
+        SceneManager.LoadScene("WelcomeScene");
+    }
+
+}
